2b4d5527822d7c4a61846d4ba54e686226def9b6
unknown <nk8713694@gmail.com>
2020-06-12 17:22:47 +0530

    process.env.PORT added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require("express")
 
 const path=require("path")
-
+const PORT=process.env.PORT||4444
 const app=express()
 
 app.use(express.json())
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/',express.static(path.join(__dirname,"public")))
 
 
-app.listen(4444,(req,res)=>{
+app.listen(PORT,(req,res)=>{
     console.log("server started on http://localhost:4444");
     
 })
